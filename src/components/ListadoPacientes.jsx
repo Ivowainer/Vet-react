d2b0d9a8334da8d275dabfde4132cee8afbf698c
Ivowainer <ivancamposwainer@gmail.com>
2022-04-01 19:14:11 -0300

    Completed form

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,21 @@
+import Pacientes from "./Pacientes"
+
 const ListadoPacientes = () => {
   return (
-    <h2>Listado Pacientes</h2>
+    <div className="md:w-1/2 lg:w-3/5">
+      <h2 className="font-black text-xl text-center">Listado Pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">Administa tus {''}
+        <span className="text-indigo-600">Pacientes y citas</span>
+      </p>
+
+      <div className="md:h-screen overflow-y-scroll">
+        <Pacientes />
+        <Pacientes />
+        <Pacientes />
+        <Pacientes />
+        <Pacientes />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Pacientes.jsx
@@ -0,0 +1,23 @@
+const Pacientes = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+          <span className="font-normal normal-case">Ivo</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+          <span className="font-normal normal-case">correo@correo.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha alta: {''}
+          <span className="font-normal normal-case">10 diciembre 2022</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+          <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eos dolorum qui placeat consectetur ratione modi, optio unde excepturi facere, accusamus porro dolore? Ea consequuntur excepturi nisi, vel rerum magnam.</span>
+        </p>
+      </div>
+  )
+}
+
+export default Pacientes
